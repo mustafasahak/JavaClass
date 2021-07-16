2ef003df583d41b9c4467a77e298bdb4838a86b6
mustafasahak <msahak@icloud.com>
2021-07-15 21:16:08 -0400

    leaned for loop in java

---
+++ src/gitdemo/ForLoopDemo.java
@@ -0,0 +1,12 @@
+package gitdemo;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("hello........" + i);
+		}
+
+	}
+
+}
